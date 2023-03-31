128d81b1fed5d0914d4cf8a4e4f2802b47d5d1db
Jessica Smith <jessica.smith@digicert.com>
2023-03-31 15:05:03 +0100

    Update API endpoint hostname

--- perfops/client.go
+++ perfops/client.go
@@ -29,7 +29,7 @@ import (
 const UserAgent = "PerfOps API Go Client"
 
 const (
-	apiRoot    = "https://api.perfops.space"
+	apiRoot    = "https://api.perfops.net"
 	basePath   = apiRoot
 	libVersion = "v1.0.0"
 	userAgent  = UserAgent + "/" + libVersion + " (" + runtime.GOOS + "/" + runtime.GOARCH + ")"
